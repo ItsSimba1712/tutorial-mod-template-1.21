ab3f15cade6f613e7d686ae8e756b6733e631640
ItsSimba1712 <itssimba1712@gmail.com>
2025-08-30 14:50:49 -0400

    Added Pink Garnet and Raw Pink Garnet

--- src/main/java/net/corvidaecrow/tutorialmod/TutorialMod.java
+++ src/main/java/net/corvidaecrow/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.corvidaecrow.tutorialmod;
 
+import net.corvidaecrow.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -7,12 +8,10 @@ import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+        ModItems.registerModItems();
 	}
 }
